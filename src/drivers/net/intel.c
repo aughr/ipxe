bb177f18f3fdac45cadbdf386773d1c937723588
Andrew Bloomgarden <andrew@newrelic.com>
2018-05-14 15:31:24 -0400

    try 2 for x557

--- src/drivers/net/intel.c
+++ src/drivers/net/intel.c
@@ -1153,6 +1153,7 @@ static struct pci_device_id intel_nics[] = {
 	PCI_ROM ( 0x8086, 0x1f41, "i354", "I354", INTEL_NO_ASDE ),
 	PCI_ROM ( 0x8086, 0x294c, "82566dc-2", "82566DC-2", 0 ),
 	PCI_ROM ( 0x8086, 0x2e6e, "cemedia", "CE Media Processor", 0 ),
+	PCI_ROM ( 0x8086, 0x1589, "x557", "X710/X557-AT", 0 ),
 };
 
 /** Intel PCI driver */
